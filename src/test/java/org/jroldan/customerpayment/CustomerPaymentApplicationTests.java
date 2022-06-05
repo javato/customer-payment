73dfece5b6f7b8a79f03ef5d474dfda9c745bd47
javato <javirolga@gmail.com>
2022-06-05 22:38:51 +0200

    initial commit

---
+++ src/test/java/org/jroldan/customerpayment/CustomerPaymentApplicationTests.java
@@ -0,0 +1,13 @@
+package org.jroldan.customerpayment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomerPaymentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
